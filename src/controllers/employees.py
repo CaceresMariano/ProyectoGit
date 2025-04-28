0db8340d80a460e114620b79e786f16df2dd58b7
CaceresMariano <mariano.caceres262@gmail.com>
2025-04-28 20:34:15 -0300

    Correccion bug funcion eliminar

--- src/controllers/employees.py
+++ src/controllers/employees.py
@@ -6,7 +6,6 @@ from src.config.constants import CHEQUEADO, NO_CHEQUEADO
 
 
 def view_employed():
-    clear_console()
     if not __check_employee():
        return print("No hay mas empleados en la base de datos.")
     employee = __find_employee()
@@ -62,29 +61,33 @@ def update_employed():
     sleep_menu(1)
 
 def delete_employed():
-    clear_console()    
-    view_employed()
-    option = int(
-        input(f"Elija el numero del empleado a eliminar [1- {len(employees_db)}]: "))
-    employees_db.pop(option - 1)
-    sleep_menu(1)
+    if __check_employee():
+        list_employeed()
+        option = int(
+            input(f"Elija el numero del empleado a eliminar [1- {len(employees_db)}]: "))
+        register_deleted = employees_db.pop(option - 1)
+        if register_deleted:
+            print(
+                f"El empleado {register_deleted["nombre"]} {register_deleted["apellido"]} fue eliminado con exito.")
+    sleep_menu(2)
 
 
 def __check_employee():
+    clear_console()
     if not employees_db:
         print("No se han cargado empleados en los registros")
         sleep_menu(2)
         return False
     return True
 
+
 def list_employeed():
-    clear_console()
+    __check_employee()
     for index, employees in enumerate(employees_db, start=1):
         print(f"{index}- {employees["nombre"]}, {employees["apellido"]}")
     
 
 def __find_employee() -> Optional[dict]:
-    __check_employee
     list_employeed()
     option = int(
         input(f"Elija un empleado de la lista [1- {len(employees_db)}]: "))
